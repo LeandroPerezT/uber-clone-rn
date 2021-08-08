73d53c33e4f1f446fa8e84a71adb8fbef64c6207
Leandro Pérez Torres <leandropereztorres98@gmail.com>
2021-08-08 14:40:28 -0300

    Setup Redux store

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
 
 export default function App() {
   return (
+    <Provider store={}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hola mamita, soy Leandro, el mas capo</Text>
     </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+// in RN, flexbox defaults to a column
