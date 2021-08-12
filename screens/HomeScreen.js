ae1f19f4e0568c2f387fe40bac1dab6b23b71669
Leandro Pérez Torres <leandropereztorres98@gmail.com>
2021-08-12 15:17:19 -0300

    Import Google Places autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar, Image} from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_API_KEY} from '@env'
 
 
 const HomeScreen = () =>{
